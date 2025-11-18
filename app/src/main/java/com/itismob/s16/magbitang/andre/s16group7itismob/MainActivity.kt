8de4b31af3534898e84a33ec520b0f65eb5e5f18
andre <165500984+roelandremagbitang@users.noreply.github.com>
2025-11-18 21:36:56 +0800

    Add files via upload

---
+++ app/src/main/java/com/itismob/s16/magbitang/andre/s16group7itismob/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.itismob.s16.magbitang.andre.s16group7itismob
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.itismob.s16.magbitang.andre.s16group7itismob.ui.theme.S16Group7ITISMOBTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            S16Group7ITISMOBTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    S16Group7ITISMOBTheme {
+        Greeting("Android")
+    }
+}
