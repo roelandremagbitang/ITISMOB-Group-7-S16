ccfdeef37a5b500b9012827393d0e372ad8b95af
andre <165500984+roelandremagbitang@users.noreply.github.com>
2025-11-18 21:37:39 +0800

    Add files via upload

---
+++ app/src/test/java/com/itismob/s16/magbitang/andre/s16group7itismob/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.itismob.s16.magbitang.andre.s16group7itismob
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
