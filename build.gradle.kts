e19ae557645c4bb2df0db9d828c9fad0f32970b1
JamesSirJames <james_matthew_perez@dlsu.edu.ph>
2025-11-21 04:18:16 +0800

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// build.gradle.kts (Project: S16Group7ITISMOB)
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+
+    // CRITICAL ADDITION: Defines the Google Services plugin version
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
